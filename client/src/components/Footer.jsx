000ed0fd7b2ae14e82ce2a12b49d00546b90873f
Limon00001 <limonhossain82@gmail.com>
2025-06-10 14:13:40 +0600

    feat(client-footer): add footer with branding and links

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,53 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 10 Jun, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+import { assets, footer_data } from '../assets/assets';
+
+const Footer = () => {
+  return (
+    <footer className="px-6 md:px-16 lg:px-24 xl:px-32 bg-primary/3">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-10 py-10 border-b border-gray-500/30 text-gray-500">
+        <div className="flex flex-col items-center md:items-start">
+          <div className="flex items-center gap-2">
+            <img src={assets.logo} alt="Logo" className="w-9 md:w-10" />
+            <p className="text-xl md:text-2xl font-semibold">EssayNorth</p>
+          </div>
+          <p className="text-center md:text-left md:max-w-[410px] mt-6">
+            Unleash the power of AI-generated content with our cutting-edge
+            platform — effortless blog creation starts here. Focus on your ideas
+            while AI handles the writing, empowering every blogger to thrive.
+          </p>
+        </div>
+
+        <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
+          {footer_data.map((section, index) => (
+            <div key={index}>
+              <h3 className="font-semibold text-base text-gray-900 md:mb-5 mb-2">
+                {section.title}
+              </h3>
+              <ul className="text-sm space-y-1">
+                {section.links.map((link, index) => (
+                  <li key={index}>
+                    <a href="#" className="hover:underline transition">
+                      {link}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <p className="p-4 text-center text-sm md:text-base text-gray-500/80">
+        Copyright 2025 &copy; Monayem Hossain Limon - All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
+
+// Export
+export default Footer;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,6 +7,7 @@
 
 // Internal Imports
 import BlogList from '../components/BlogList';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
 import Newsletter from '../components/Newsletter';
@@ -18,6 +19,7 @@ const Home = () => {
       <Header />
       <BlogList />
       <Newsletter />
+      <Footer />
     </>
   );
 };
