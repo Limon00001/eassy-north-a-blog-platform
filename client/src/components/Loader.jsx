cc5193093283ef7d8a8ecfb9a24036806fe48f0f
Limon00001 <limonhossain82@gmail.com>
2025-06-10 15:30:09 +0600

    feat(client-loader): add loader component for loading state indication

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,17 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 10 Jun, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+const Loader = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="animate-spin rounded-full h-16 w-16 border-t-white border-4 border-gray-700"></div>
+    </div>
+  );
+};
+
+// Export
+export default Loader;
