bf91d2ab5ca72c482d226d409603c07c1d2a4ab8
Limon00001 <limonhossain82@gmail.com>
2025-06-11 10:32:25 +0600

    fix(client-admin-add-blog): remove duplicate name attribute from category select element

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -140,7 +140,6 @@ const AddBlog = () => {
         <select
           name="category"
           id="category"
-          name="category"
           className="mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded"
           value={formData.category}
           onChange={handleInputChange}
