a91131150f9b07d6e7cfac2b452b7088155a6717
Limon00001 <limonhossain82@gmail.com>
2025-06-11 11:33:50 +0600

    feat(server-admin-auth): implement admin login functionality

---
+++ server/controllers/adminController.js
@@ -0,0 +1,40 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 11 Jun, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import jwt from 'jsonwebtoken';
+
+// Login Functionality for Admin
+const adminLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (
+      email !== process.env.ADMIN_EMAIL &&
+      password !== process.env.ADMIN_PASSWORD
+    ) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid credentials',
+      });
+    }
+
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: 'Login successful',
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { adminLogin };
---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Monayem Hossain Limon
+ * GitHub: https://github.com/Limon00001
+ * Date: 11 Jun, 2025
+ * @copyright 2025 monayem_hossain_limon
+ */
+
+// External Imports
+import express from 'express';
+
+// Internal Imports
+import { adminLogin } from '../controllers/adminController.js';
+
+// Router
+const router = express.Router();
+
+// Routes
+router.post('/login', adminLogin);
+
+// Export
+export default router;
