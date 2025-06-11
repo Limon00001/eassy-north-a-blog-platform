4ef8cab68df9356713ab17d019af53e5d385b8f4
Limon00001 <limonhossain82@gmail.com>
2025-06-11 09:30:30 +0600

    feat(admin-list-blog): implement blog listing with data fetching and table display

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -5,8 +5,67 @@
  * @copyright 2025 monayem_hossain_limon
  */
 
+// External Imports
+import { useEffect, useState } from 'react';
+
+// Internal Imports
+import { blog_data } from '../../assets/assets';
+import BlogTableItem from '../../components/admin/BlogTableItem';
+
+// Component
 const ListBlog = () => {
-  return <div>ListBlog</div>;
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div className="flex-1 pt-5 px-5 md:pt-12 md:pl-16 bg-blue-50/50">
+      <h1 className="">All blogs</h1>
+
+      {/* Blog Table */}
+      <div className="relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-600 uppercase text-left">
+            <tr>
+              <th scope="col" className="px-2 py-4 xl:px-6">
+                #
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Blog Title
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Date
+              </th>
+              <th scope="col" className="px-2 py-4 max-sm:hidden">
+                Status
+              </th>
+              <th scope="col" className="px-2 py-4">
+                Action
+              </th>
+            </tr>
+          </thead>
+
+          {/* Blog Table Body with data */}
+          <tbody>
+            {blogs.map((blog, index) => (
+              <BlogTableItem
+                key={blog._id}
+                blog={blog}
+                fetchBlogs={fetchBlogs}
+                index={index + 1}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 // Export
